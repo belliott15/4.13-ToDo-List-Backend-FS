a35a2f74712fb93ec685e1a906805a45fcec3240
Beau Elliott <belliott1516@gmail.com>
2022-06-28 16:10:55 -0700

    added test to create new user, created users controller user model and user services and added controller to app.js

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -3,13 +3,27 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const robotUser = {
+  email: 'mr.roboto@sentient.robot',
+  password: '123456'
+}
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('POST / creates a new user', () => {
+    const res = await request(app)
+    .post('/api/v1/users')
+    .send(robotUser);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email
+    });
   });
+
   afterAll(() => {
     pool.end();
   });
